ee961ace3c51a745918ab4f82a11346c47669a5f
Mr_Sh_? <sh.muhammad.itn1@gmail.com>
2026-01-20 22:01:30 +0500

    header commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,40 +9,46 @@ function App() {
   const { gameOver, winner, round, dispatch, score } = useContext(UserContext);
 
   return (
-    <div className="container">
-      {gameOver ? (
-        <div className="winner-screen">
-          <div className="winner-inner">
-            <div className="winner-icon">🏆</div>
-            <h1 className="winner-title">
-              WINNER IS {winner.toUpperCase()} TEAM!
-            </h1>
-            <div className="winner-score">WITH {score[winner]} POINTS</div>
-            <p className="winner-rounds">Rounds played: {round - 1}</p>
-            <button
-              className="restart-btn"
-              onClick={() => dispatch({ type: "restart" })}
-            >
-              Restart Game
-            </button>
-          </div>
-        </div>
-      ) : (
-        <div className="inner">
-          <div className="game-header">
-            <h1>ROUND {round}</h1>
-          </div>
-          <div className="app">
-            <TeamPanel command="left" color="blue" />
-            <div>
-              <Timer />
-              <Rope />
+    <div className="main">
+      <div className="container">
+        {gameOver ? (
+          <div className="winner-screen">
+            <div className="winner-inner">
+              <div className="winner-icon">🏆</div>
+              <h1 className="winner-title">
+                WINNER IS {winner.toUpperCase()} TEAM!
+              </h1>
+              <div className="winner-score">WITH {score[winner]} POINTS</div>
+              <p className="winner-rounds">Rounds played: {round - 1}</p>
+              <button
+                className="restart-btn"
+                onClick={() => dispatch({ type: "restart" })}
+              >
+                Restart Game
+              </button>
             </div>
-            <div className="straight-line"></div>
-            <TeamPanel command="right" color="red" />
           </div>
-        </div>
-      )}
+        ) : (
+          <div className="inner">
+            <div className="game-header">
+              <div className="header-center">
+                <h1>TUG OF MATH</h1>
+              </div>
+                <Timer />
+
+              <h1>ROUND {round}</h1>
+            </div>
+            <div className="app">
+              <TeamPanel command="left" color="blue" />
+              <div>
+                <Rope />
+              </div>
+              <div className="straight-line"></div>
+              <TeamPanel command="right" color="red" />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
